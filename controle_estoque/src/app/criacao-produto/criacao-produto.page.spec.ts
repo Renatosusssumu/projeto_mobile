5f7dfe3aa63e283c0993ff5388b6dd0f6c1fa0c1
RenatoSussumu <renatosussumutakahashijunior@gmail.com>
2024-09-22 19:55:56 -0300

    criacao do forms da criacao-produto

--- controle_estoque/src/app/criacao-produto/criacao-produto.page.spec.ts
+++ controle_estoque/src/app/criacao-produto/criacao-produto.page.spec.ts
@@ -1,11 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterModule } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
+
 import { CriacaoProdutoPage } from './criacao-produto.page';
 
 describe('CriacaoProdutoPage', () => {
   let component: CriacaoProdutoPage;
   let fixture: ComponentFixture<CriacaoProdutoPage>;
 
-  beforeEach(() => {
+  beforeEach(async () => {
+
+    await TestBed.configureTestingModule({
+      declarations: [CriacaoProdutoPage],
+      imports: [IonicModule.forRoot(), RouterModule.forRoot([])]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(CriacaoProdutoPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
