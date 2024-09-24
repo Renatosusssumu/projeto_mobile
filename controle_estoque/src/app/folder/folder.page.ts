5e2686131a2ff10ade5db10299724f272234d466
RenatoSussumu <renatosussumutakahashijunior@gmail.com>
2024-09-24 18:21:07 -0300

    criaçao dos forms e dos modais

--- controle_estoque/src/app/folder/folder.page.ts
+++ controle_estoque/src/app/folder/folder.page.ts
@@ -1,5 +1,7 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { IonModal, NavController } from '@ionic/angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-folder',
@@ -7,11 +9,32 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./folder.page.scss'],
 })
 export class FolderPage implements OnInit {
+  @ViewChild(IonModal, { static: false }) modal!: IonModal;  // Agora o segundo argumento está presente
+  alertButtons = ['OK'];
   public folder!: string;
-  private activatedRoute = inject(ActivatedRoute);
-  constructor() {}
+  criacaoestForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private navCtrl: NavController, private activatedRoute: ActivatedRoute) {
+    this.criacaoestForm = this.fb.group({
+      nomeest: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    if (this.criacaoestForm.valid) {
+      this.navCtrl.navigateForward('/folder', {
+        queryParams: this.criacaoestForm.value
+      });
+    }
+  }
 
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id') as string;
+  }
+  
+  cancel() {
+    if (this.modal) {
+      this.modal.dismiss();  // Fechar o modal
+    }
   }
 }
